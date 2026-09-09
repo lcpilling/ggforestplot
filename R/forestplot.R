c36da700d284a61c059aed4260255924e5dbb6f3
Luke C. Pilling <luke.pilling@gmail.com>
2026-09-09 14:36:42 +0100

    Revert "If integers in table add commas"
    This reverts commit 1e017b57c9cd02a08c0f6b1430b5c6acbb5625e8.

--- R/forestplot.R
+++ R/forestplot.R
@@ -338,7 +338,6 @@ forestplot <- function(df,
       vals <- label_data[[i]]
       max_w <- max(nchar(vals), na.rm = TRUE)
       align_flag <- if (col_is_integer[i]) "" else "-"
-      vals <- scales::comma(vals) # if integer then format with commas
       formatC(vals, width = max_w, flag = align_flag)
     })
 
